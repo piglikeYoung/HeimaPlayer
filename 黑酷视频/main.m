543f62a08d5a6e4703db2a5a4726ff2e44389433
piglikeyoung <316046510@qq.com>
2015-03-24 13:13:15 +0800

    Initial Commit

---
+++ 黑酷视频/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  黑酷视频
+//
+//  Created by piglikeyoung on 15/3/24.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
